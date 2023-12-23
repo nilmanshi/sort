2cb69c5b42fa39873fc0e9aad1bdc38ac637b3bb
nilmanshi <106310436+nilmanshi@users.noreply.github.com>
2023-12-23 21:09:04 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print(vector<int> &v) {
+  for (int i = 0; i < v.size(); ++i) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+}
+void bubbleSort(vector<int> &v) {
+  int n = v.size();
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n - 1; ++j) {
+      if (v[j] > v[j + 1]) {
+        swap(v[j], v[j + 1]);
+      }
+    }
+  }
+}
+
+int main() {
+  vector<int> v = {5, 4, 3, 2, 1};
+  bubbleSort(v);
+  print(v);
+  return 0;
+}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print(vector<int> &v) {
+  for (int i = 0; i < v.size(); ++i) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+}
+void bubbleSort(vector<int> &v) {
+  int n = v.size();
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n - 1; ++j) {
+      if (v[j] < v[j + 1]) {
+        swap(v[j], v[j + 1]);
+      }
+    }
+  }
+}
+
+int main() {
+  vector<int> v = {5, 4, 9, 2, 1};
+  bubbleSort(v);
+  print(v);
+  return 0;
+}
